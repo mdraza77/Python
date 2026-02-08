fcccc26bf79fca15f85361d33fb68d9aa91d4f51
Md Raza <mdraza8397@gmail.com>
2026-02-08 22:58:50 +0530

    dice rolling game done

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,10 +1,13 @@
 import random
-inp = input("Roll the dice (y/n)?")
-if inp == 'y' or inp == 'Y':
-    random_number1 = random.randint(1, 6)
-    random_number2 = random.randint(1, 6)
-    print(random_number1, random_number2)
-elif (inp == 'n' or inp == 'N'):
-    print("Thank you!")
-else:
-    print("Invalid feedback!")
+
+while True:
+    choice = input("Roll the dice (y/n)?").lower()
+    if (choice == 'y'):
+        random_number1 = random.randint(1, 6)
+        random_number2 = random.randint(1, 6)
+        print(random_number1, random_number2)
+    elif (choice == 'n'):
+        print("Thank you!")
+        break
+    else:
+        print("Invalid choice!")
